11da4ea0819930102093c8f88083274223ab164f
m1ster_br0bot <soupy.42@gmail.com>
2020-12-10 22:58:15 -0500

    Add files via upload

--- Jane.py
+++ Jane.py
@@ -9,6 +9,16 @@ import webbrowser
 
 my_file = "contacts.txt"
 
+def banner():
+	print("""
+     _                                 Virtual
+    | | __ _ _ __   ___   _ __  _   _  Assistant
+ _  | |/ _` | '_ \ / _ \ | '_ \| | | |
+| |_| | (_| | | | |  __/_| |_) | |_| |
+ \___/ \__,_|_| |_|\___(_) .__/ \__, |
+                         |_|    |___/ 
+	""")
+# initialization
 
 def speak(audioString):
     print(audioString)
@@ -20,7 +30,8 @@ def recordAudio():
     # Record Audio
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Say something!")
+        banner()
+        print("Say something:")
         audio = r.listen(source)
     
         # Speech recognition using Google Speech Recognition
@@ -41,7 +52,7 @@ def sendSMS():
     numbers = []
     names = []
     speak("Select acontact by number: ")   
-    #my_file = "contacts.txt"
+
         # open file in read mode
     with open(my_file, 'r') as file_handle:
             # convert file contents into a list
@@ -55,6 +66,7 @@ def sendSMS():
             if(i + 1 >= len(lines)):
                 break
                 my_file.close()
+    
     data = recordAudio()        
     i = data
     i = int(i)
@@ -91,7 +103,6 @@ def jarvis(data):
     if "start" in data:
         data = data.split(" ", 1)
         start = data[1]
-        start = start.lower()
         speak("Starting " + start)
         os.system(start + "&")
         
@@ -106,78 +117,11 @@ def jarvis(data):
         
     if "open Instagram" in data:
         instagram = 'istekram'
-        os.system(istekram + "&")
+        os.system(instagram + "&")
     
 # initialization
 time.sleep(.5)
-speak("Hi Tim, how can I help?")
-while 1:
-    data = recordAudio()
-    jarvis(data)
-
-            #split each line and appends name and number to respective list                
-            person = val.split(" ", 1)
-            names.append(person[0])
-            numbers.append(person[1])
-            print(i, names[i], numbers[i])
-            if(i + 1 >= len(lines)):
-                break
-    speak("Select acontact by number: ")
-    data = recordAudio()        
-    i = data
-    i = int(i)
-    dest = numbers[i]
-    speak("Record your message")
-    data = recordAudio()
-    message = data
-    speak("Would you like to send " + message + " to " + names[i] + "?")
-    data = recordAudio()
-    if "yes" in data:
-        os.system("kdeconnect-cli --send-sms '%s' -n s20 --destination %s"  % (message, dest))
-        speak("Message sent to " + names[i])
-
-def jarvis(data):
-    if "how are you" in data:
-        speak("I am fine, thanks")
-
-    if "what time is it" in data:
-        print(ctime())
-        #speak(ctime())
-
-    if "where is" in data:
-        data = data.split(" ", 2)
-        location = data[2]
-        speak("Hold on Tim, I will show you where " + location + " is.")
-        webbrowser.open_new_tab("https://www.google.com/maps/place/" + location + "/&amp;")
-        
-    if "search for" in data:
-        data = data.split(" ", 2)
-        search = data[2]
-        speak("Hold on Tim, I will search for " + search)
-        webbrowser.open_new_tab('http://www.google.com/search?btnG=1&q=' + search)
-        
-    if "start" in data:
-        data = data.split(" ", 1)
-        start = data[1]
-        start = start.lower()
-        speak("Starting " + start)
-        os.system(start + "&")
-        
-    if "signal" in data:
-        os.system("signal-desktop &")
-        
-    if "hey Jane" in data:
-        speak("Hey Tim, what's up?")
-        
-    if "send text" in data:
-        sendSMS()
-        
-    if "open Instagram" in data:
-        instagram = 'istekram'
-        os.system(istekram + "&")
-    
-# initialization
-time.sleep(.5)
+banner()
 speak("Hi Tim, how can I help?")
 while 1:
     data = recordAudio()
